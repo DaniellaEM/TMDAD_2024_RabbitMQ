996f053d0e6974cd19737dedfe824105f5e1ae4f
DaniellaEM <905938@unizar.es>
2024-05-16 17:11:35 +0200

    Filtrar y ordenar topics y probar si la otra cola esta escuchando

--- src/main/kotlin/com/TMDAD_2024/rabbitmq/RabbitMqConsumer.kt
+++ src/main/kotlin/com/TMDAD_2024/rabbitmq/RabbitMqConsumer.kt
@@ -6,24 +6,31 @@ import org.springframework.stereotype.Service
 import org.springframework.messaging.simp.SimpMessagingTemplate
 
 @Service
-class RabbitMqConsumer(private val messagingTemplate: SimpMessagingTemplate, private val rabbitTemplate: RabbitTemplate) {
+class RabbitMqConsumer(private val messagingTemplate: SimpMessagingTemplate,
+                       private val rabbitTemplate: RabbitTemplate) {companion object {private val trendMap = mutableMapOf<String, Int>()}
 
-    private val trendMap = mutableMapOf<String, Int>()
     @RabbitListener(queues = ["MESSAGE_QUEUE"])
     fun consume(message: String) {
         println("Received message -> $message")
 
-        // Analizar el mensaje para extraer las palabras clave
-        val words = message.split("\\s+")
+        // Procesar el mensaje para extraer las palabras clave
+        val words = processText(message)
 
-        // Actualizar la lista de tendencias
-        for (word in words) {
-            trendMap[word] = trendMap.getOrDefault(word, 0) + 1
+        // Actualizar la lista de tendencias global
+        synchronized(trendMap) {
+            for (word in words) {
+                trendMap[word] = trendMap.getOrDefault(word, 0) + 1
+            }
+        }
+
+        // Obtener las tendencias actuales
+        val currentTrends = synchronized(trendMap) {
+            trendMap.toList().sortedByDescending { it.second }
         }
 
         // Imprimir la lista de tendencias
         println("Tendencias actuales:")
-        trendMap.forEach { (word, count) ->
+        currentTrends.forEach { (word, count) ->
             println("$word: $count")
         }
 
@@ -33,4 +40,24 @@ class RabbitMqConsumer(private val messagingTemplate: SimpMessagingTemplate, pri
         // Send the received message to the new queue
         rabbitTemplate.convertAndSend("SECOND_MESSAGE_QUEUE", message)
     }
+
+    private fun processText(text: String): List<String> {
+        // Filtrar palabras permitidas y eliminar signos de puntuación
+        val filteredWords = text.split(Regex("\\s+"))
+                .map { it.replace(Regex("[,.?!]"), "") }
+                .filter { it.matches(Regex("[a-zA-ZáéíóúüÁÉÍÓÚÜñÑ]+")) }
+
+        // Contar la frecuencia de cada palabra
+        val wordCounts = filteredWords.groupingBy { it }.eachCount()
+
+        return wordCounts.toList().sortedByDescending { it.second }.map { it.first }
+    }
 }
+//@Service
+//class SecondQueueConsumer {
+//
+//    @RabbitListener(queues = ["SECOND_MESSAGE_QUEUE"])
+//    fun consume(message: String) {
+//        println("Received message from SECOND_MESSAGE_QUEUE -> $message")
+//    }
+//}
