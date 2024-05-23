92134d4971eb5f9fd49e81992fd5c91008da0d37
Guillermo Cruz <gcruz@etiqmedia.com>
2024-05-23 22:48:54 +0200

    Limpieza codigo

--- src/main/kotlin/com/TMDAD_2024_RabbitMQ/rabbitmq/RabbitMqConsumer.kt
+++ src/main/kotlin/com/TMDAD_2024_RabbitMQ/rabbitmq/RabbitMqConsumer.kt
@@ -2,14 +2,11 @@ package com.TMDAD_2024_RabbitMQ.rabbitmq
 
 import org.springframework.amqp.rabbit.annotation.RabbitListener
 import org.springframework.amqp.rabbit.core.RabbitTemplate
-import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 import org.springframework.scheduling.annotation.EnableScheduling
 import org.springframework.scheduling.annotation.Scheduled
 import java.io.File
 import java.util.*
-
-data class MyMessage(val list: List<String>)
 
 @EnableScheduling
 @Service
@@ -75,37 +72,11 @@ class RabbitMqConsumer(private val rabbitTemplate: RabbitTemplate) {
     @RabbitListener(queues = ["MESSAGE_QUEUE"])
     fun consume(message: String) {
         println("Received message -> $message")
-
         // Procesar el mensaje para extraer las palabras clave
         val words = processText(message)
-
-//        // Actualizar la lista de tendencias global
-//        synchronized(trendMap) {
-//            for (word in words) {
-//                trendMap[word] = trendMap.getOrDefault(word, 0) + 1
-//            }
-//        }
+        //Añadir a la lista interna
         add(words)
-
-//        // Obtener las tendencias actuales
-//        val currentTrends = synchronized(trendMap) {
-//            trendMap.toList().sortedByDescending { it.second }
-//        }
-//
-//        // Imprimir la lista de tendencias
-//        println("Tendencias actuales:")
-//        currentTrends.forEach { (word, count) ->
-//            println("$word: $count")
-//        }
-//
-//        // Formar un mensaje único con las palabras clave ordenado
-//        val trendingTopicsMessage = currentTrends.mapIndexed { index, pair ->
-//            "${index + 1}.º ${pair.first}: ${pair.second}"
-//        }.joinToString(separator = ", ")
-//
-//
-//        // Enviar los trending topics a la nueva cola
-//        rabbitTemplate.convertAndSend("SECOND_MESSAGE_QUEUE", trendingTopicsMessage)
+        //Enviar a la cola de trendings
         sendToRabbit(rabbitTemplate)
     }
 
